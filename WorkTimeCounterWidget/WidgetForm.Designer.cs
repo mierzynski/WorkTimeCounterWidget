511523d417e5196e898100b585a9ff3f6e6618b4
mierzynski <mmierzynski@icloud.com>
2025-01-03 15:21:10 +0100

    Added colors to buttons and secured changing designs while counting time

--- WorkTimeCounterWidget/WidgetForm.Designer.cs
+++ WorkTimeCounterWidget/WidgetForm.Designer.cs
@@ -43,8 +43,9 @@
             // 
             // label_ProjectName
             // 
+            label_ProjectName.AutoEllipsis = true;
             label_ProjectName.AutoSize = true;
-            label_ProjectName.Location = new Point(12, 13);
+            label_ProjectName.Location = new Point(27, 13);
             label_ProjectName.Name = "label_ProjectName";
             label_ProjectName.Size = new Size(25, 15);
             label_ProjectName.TabIndex = 0;
@@ -53,7 +54,7 @@
             // label_ProjectTime
             // 
             label_ProjectTime.AutoSize = true;
-            label_ProjectTime.Location = new Point(43, 13);
+            label_ProjectTime.Location = new Point(58, 13);
             label_ProjectTime.Name = "label_ProjectTime";
             label_ProjectTime.Size = new Size(49, 15);
             label_ProjectTime.TabIndex = 1;
@@ -61,22 +62,30 @@
             // 
             // button_StartStop
             // 
-            button_StartStop.BackColor = Color.Gray;
-            button_StartStop.ForeColor = SystemColors.ControlText;
-            button_StartStop.Location = new Point(140, 5);
+            button_StartStop.BackColor = Color.Red;
+            button_StartStop.FlatAppearance.BorderColor = Color.Red;
+            button_StartStop.FlatAppearance.BorderSize = 0;
+            button_StartStop.FlatStyle = FlatStyle.Flat;
+            button_StartStop.ForeColor = Color.White;
+            button_StartStop.Location = new Point(175, 5);
+            button_StartStop.Margin = new Padding(0);
             button_StartStop.Name = "button_StartStop";
-            button_StartStop.Size = new Size(53, 31);
+            button_StartStop.Size = new Size(60, 31);
             button_StartStop.TabIndex = 2;
-            button_StartStop.Text = "stop";
+            button_StartStop.Text = "PAUSED";
             button_StartStop.UseVisualStyleBackColor = false;
             button_StartStop.Click += button_StartStop_Click;
             // 
             // button_Break
             // 
-            button_Break.BackColor = Color.Gray;
+            button_Break.BackColor = Color.FromArgb(64, 64, 64);
             button_Break.BackgroundImage = (Image)resources.GetObject("button_Break.BackgroundImage");
             button_Break.BackgroundImageLayout = ImageLayout.Center;
-            button_Break.Location = new Point(199, 5);
+            button_Break.FlatAppearance.BorderColor = Color.White;
+            button_Break.FlatAppearance.BorderSize = 0;
+            button_Break.FlatStyle = FlatStyle.Flat;
+            button_Break.Location = new Point(235, 5);
+            button_Break.Margin = new Padding(0);
             button_Break.Name = "button_Break";
             button_Break.Size = new Size(31, 31);
             button_Break.TabIndex = 3;
@@ -85,10 +94,14 @@
             // 
             // button_Infolinia
             // 
-            button_Infolinia.BackColor = Color.Gray;
+            button_Infolinia.BackColor = Color.FromArgb(64, 64, 64);
             button_Infolinia.BackgroundImage = (Image)resources.GetObject("button_Infolinia.BackgroundImage");
             button_Infolinia.BackgroundImageLayout = ImageLayout.Center;
-            button_Infolinia.Location = new Point(235, 5);
+            button_Infolinia.FlatAppearance.BorderColor = Color.White;
+            button_Infolinia.FlatAppearance.BorderSize = 0;
+            button_Infolinia.FlatStyle = FlatStyle.Flat;
+            button_Infolinia.Location = new Point(266, 5);
+            button_Infolinia.Margin = new Padding(0);
             button_Infolinia.Name = "button_Infolinia";
             button_Infolinia.Size = new Size(31, 31);
             button_Infolinia.TabIndex = 4;
@@ -102,10 +115,14 @@
             // 
             // button_ShowMainWindow
             // 
-            button_ShowMainWindow.BackColor = Color.Gray;
+            button_ShowMainWindow.BackColor = Color.FromArgb(64, 64, 64);
             button_ShowMainWindow.BackgroundImage = (Image)resources.GetObject("button_ShowMainWindow.BackgroundImage");
             button_ShowMainWindow.BackgroundImageLayout = ImageLayout.Center;
-            button_ShowMainWindow.Location = new Point(271, 5);
+            button_ShowMainWindow.FlatAppearance.BorderColor = Color.White;
+            button_ShowMainWindow.FlatAppearance.BorderSize = 0;
+            button_ShowMainWindow.FlatStyle = FlatStyle.Flat;
+            button_ShowMainWindow.Location = new Point(297, 5);
+            button_ShowMainWindow.Margin = new Padding(0);
             button_ShowMainWindow.Name = "button_ShowMainWindow";
             button_ShowMainWindow.Size = new Size(20, 31);
             button_ShowMainWindow.TabIndex = 5;
@@ -113,33 +130,40 @@
             // 
             // button_Up_Click
             // 
+            button_Up_Click.BackColor = Color.FromArgb(64, 64, 64);
             button_Up_Click.BackgroundImage = (Image)resources.GetObject("button_Up_Click.BackgroundImage");
             button_Up_Click.BackgroundImageLayout = ImageLayout.Center;
-            button_Up_Click.Location = new Point(101, 5);
+            button_Up_Click.FlatAppearance.BorderSize = 0;
+            button_Up_Click.FlatStyle = FlatStyle.Flat;
+            button_Up_Click.Location = new Point(4, 5);
             button_Up_Click.Margin = new Padding(0);
             button_Up_Click.Name = "button_Up_Click";
-            button_Up_Click.Size = new Size(36, 16);
+            button_Up_Click.Size = new Size(20, 16);
             button_Up_Click.TabIndex = 6;
-            button_Up_Click.UseVisualStyleBackColor = true;
+            button_Up_Click.UseVisualStyleBackColor = false;
             button_Up_Click.Click += button_Up_Click_Click;
             // 
             // button_Down_Click
             // 
+            button_Down_Click.BackColor = Color.FromArgb(64, 64, 64);
             button_Down_Click.BackgroundImage = (Image)resources.GetObject("button_Down_Click.BackgroundImage");
             button_Down_Click.BackgroundImageLayout = ImageLayout.Center;
-            button_Down_Click.Location = new Point(101, 20);
+            button_Down_Click.FlatAppearance.BorderSize = 0;
+            button_Down_Click.FlatStyle = FlatStyle.Flat;
+            button_Down_Click.Location = new Point(4, 20);
             button_Down_Click.Margin = new Padding(0);
             button_Down_Click.Name = "button_Down_Click";
-            button_Down_Click.Size = new Size(36, 16);
+            button_Down_Click.Size = new Size(20, 16);
             button_Down_Click.TabIndex = 7;
-            button_Down_Click.UseVisualStyleBackColor = true;
+            button_Down_Click.UseVisualStyleBackColor = false;
             button_Down_Click.Click += button_Down_Click_Click;
             // 
             // WidgetForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(296, 39);
+            BackColor = Color.FromArgb(244, 244, 244);
+            ClientSize = new Size(320, 39);
             Controls.Add(button_Down_Click);
             Controls.Add(button_Up_Click);
             Controls.Add(button_ShowMainWindow);
--- WorkTimeCounterWidget/WidgetForm.cs
+++ WorkTimeCounterWidget/WidgetForm.cs
@@ -1,113 +1,4 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.ComponentModel;
-//using System.Data;
-//using System.Drawing;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using System.Windows.Forms;
-
-//namespace WorkTimeCounterWidget
-//{
-//    public partial class WidgetForm : Form
-//    {
-//        private TimeSpan projectTime = TimeSpan.Zero;  // Czas dla projektu
-//        private TimeSpan breakTime = TimeSpan.Zero;    // Czas dla przerwy
-//        private bool isProjectRunning = false;          // Status czy stoper dla projektu jest uruchomiony
-//        private bool isBreakRunning = false;            // Status czy przerwa jest uruchomiona
-//        private bool isTimerRunning = false;            // Status ogólny timera (czy zlicza czas)
-
-//        public WidgetForm()
-//        {
-//            InitializeComponent();
-//        }
-
-//        private void button_StartStop_Click(object sender, EventArgs e)
-//        {
-//            if (isTimerRunning)
-//            {
-//                // Zatrzymujemy stoper dla projektu
-//                timer.Stop();
-//                isProjectRunning = false;
-
-//                // Zmieniamy tekst i kolor przycisku
-//                button_StartStop.Text = "Paused";
-//                button_StartStop.BackColor = Color.Red;
-//            }
-//            else
-//            {
-//                // Rozpoczynamy stoper dla projektu
-//                timer.Start();
-//                isProjectRunning = true;
-
-//                // Zmieniamy tekst i kolor przycisku
-//                button_StartStop.Text = "Started";
-//                button_StartStop.BackColor = Color.Green;
-//            }
-//            isTimerRunning = !isTimerRunning;  // Przełączamy stan timera
-//        }
-
-
-//        private void button_Break_Click(object sender, EventArgs e)
-//        {
-//            if (isBreakRunning)
-//            {
-//                // Wznawiamy stoper dla projektu
-//                timer.Start();
-//                isBreakRunning = false;
-
-//                // Zmieniamy tekst i kolor przycisku
-//                button_Break.Text = "Break";
-//                button_Break.BackColor = Color.Red;
-
-//                // Zmieniamy stan przycisku Start/Stop
-//                button_StartStop.Text = "Started";
-//                button_StartStop.BackColor = Color.Green;
-//            }
-//            else
-//            {
-//                // Pauzujemy czas dla projektu i zaczynamy przerwę
-//                timer.Stop();
-//                isBreakRunning = true;
-
-//                // Zmieniamy tekst i kolor przycisku przerwy
-//                button_Break.Text = "Resume";
-//                button_Break.BackColor = Color.Green;
-
-//                // Zmieniamy stan przycisku Start/Stop
-//                button_StartStop.Text = "Paused";
-//                button_StartStop.BackColor = Color.Red;
-//            }
-//        }
-
-
-//        private void button_Infolinia_Click(object sender, EventArgs e)
-//        {
-
-//        }
-
-//        private void timer_Tick(object sender, EventArgs e)
-//        {
-//            if (isProjectRunning)
-//            {
-//                // Zliczamy czas dla projektu
-//                projectTime = projectTime.Add(TimeSpan.FromSeconds(1));
-//                label_ProjectTime.Text = projectTime.ToString(@"hh\:mm\:ss");
-//            }
-
-//            if (isBreakRunning)
-//            {
-//                // Zliczamy czas na przerwie
-//                breakTime = breakTime.Add(TimeSpan.FromSeconds(1));
-//                label_ProjectTime.Text = breakTime.ToString(@"hh\:mm\:ss");  // Przerwa wyświetlana w tej samej etykiecie
-//            }
-//        }
-//    }
-//}
-
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
@@ -144,16 +35,33 @@ namespace WorkTimeCounterWidget
             }
         }
 
-
         private void UpdateCurrentProject()
         {
             if (currentProjectIndex >= 0 && currentProjectIndex < projects.Count)
             {
                 var currentProject = projects[currentProjectIndex];
+
+                // Ustaw nazwę projektu
                 label_ProjectName.Text = currentProject.Name;
+
+                // Ustaw czas projektu
                 label_ProjectTime.Text = currentProject.TimeSpent.ToString(@"hh\:mm\:ss");
+
+                // Dynamicznie dostosuj pozycję label_ProjectTime w stosunku do label_ProjectName
+                label_ProjectTime.Left = label_ProjectName.Right + 3;
+            }
+            else
+            {
+                // Jeśli nie ma projektu, wyświetl komunikat
+                label_ProjectName.Text = "No project selected";
+                label_ProjectTime.Text = "00:00:00";
+
+                // Dostosuj label_ProjectTime w przypadku braku projektu
+                label_ProjectTime.Left = label_ProjectName.Right + 3;
             }
         }
+
+
 
 
         private void button_StartStop_Click(object sender, EventArgs e)
@@ -162,15 +70,15 @@ namespace WorkTimeCounterWidget
             {
                 timer.Stop();
                 isProjectRunning = false;
-                button_StartStop.Text = "Paused";
-                button_StartStop.BackColor = Color.Red;
+                button_StartStop.Text = "PAUSED";
+                button_StartStop.BackColor = ColorTranslator.FromHtml("#FF0000");
             }
             else
             {
                 timer.Start();
                 isProjectRunning = true;
-                button_StartStop.Text = "Started";
-                button_StartStop.BackColor = Color.Green;
+                button_StartStop.Text = "STARTED";
+                button_StartStop.BackColor = ColorTranslator.FromHtml("#008000");
             }
 
             isTimerRunning = !isTimerRunning;
@@ -184,12 +92,11 @@ namespace WorkTimeCounterWidget
                 isBreakRunning = false;
 
                 // Zmieniamy tekst i kolor przycisku
-                button_Break.Text = "Break";
-                button_Break.BackColor = Color.Red;
+                button_Break.BackColor = Color.FromArgb(64,64,64);
 
                 // Zmieniamy stan przycisku Start/Stop
-                button_StartStop.Text = "Started";
-                button_StartStop.BackColor = Color.Green;
+                button_StartStop.Text = "STARTED";
+                button_StartStop.BackColor = ColorTranslator.FromHtml("#008000");
             }
             else
             {
@@ -198,12 +105,11 @@ namespace WorkTimeCounterWidget
                 isBreakRunning = true;
 
                 // Zmieniamy tekst i kolor przycisku przerwy
-                button_Break.Text = "Resume";
-                button_Break.BackColor = Color.Green;
+                button_Break.BackColor = ColorTranslator.FromHtml("#008000");
 
                 // Zmieniamy stan przycisku Start/Stop
-                button_StartStop.Text = "Paused";
-                button_StartStop.BackColor = Color.Red;
+                button_StartStop.Text = "PAUSED";
+                button_StartStop.BackColor = ColorTranslator.FromHtml("#FF0000");
             }
         }
         private void button_Infolinia_Click(object sender, EventArgs e)
@@ -222,7 +128,7 @@ namespace WorkTimeCounterWidget
 
         private void button_Up_Click_Click(object sender, EventArgs e)
         {
-            if (projects.Count == 0) return;
+            if (projects.Count == 0 || isTimerRunning) return;  // Sprawdzamy, czy czas jest aktywny
 
             currentProjectIndex = (currentProjectIndex - 1 + projects.Count) % projects.Count;
             UpdateCurrentProject();
@@ -230,10 +136,11 @@ namespace WorkTimeCounterWidget
 
         private void button_Down_Click_Click(object sender, EventArgs e)
         {
-            if (projects.Count == 0) return;
+            if (projects.Count == 0 || isTimerRunning) return;  // Sprawdzamy, czy czas jest aktywny
 
             currentProjectIndex = (currentProjectIndex + 1) % projects.Count;
             UpdateCurrentProject();
         }
+
     }
 }
